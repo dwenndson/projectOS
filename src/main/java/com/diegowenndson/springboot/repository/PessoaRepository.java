b5b078db7466780d76e1b69139da0cdd854200a0
Diego Wenndson Souza Costa <CIANDT\dwenndson@lnb029358bhz.ciandt.global>
2022-09-28 16:05:40 -0300

    Implementado repositorio de Pessoa

--- src/main/java/com/diegowenndson/springboot/repository/PessoaRepository.java
+++ src/main/java/com/diegowenndson/springboot/repository/PessoaRepository.java
@@ -1,2 +1,10 @@
-package com.diegowenndson.springboot.repository;public interface PessoaRepository {
+package com.diegowenndson.springboot.repository;
+
+import com.diegowenndson.springboot.domain.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
+
+    Pessoa findByCpf(@Param("cpf") String cpf);
 }
