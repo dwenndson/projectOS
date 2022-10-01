05615e88226bc86f1ecf9fcf263ea1c844387b89
Diego Wenndson Souza Costa <CIANDT\dwenndson@lnb029358bhz.ciandt.global>
2022-10-01 16:27:16 -0300

    terminando todos os serviços de OS

--- src/main/java/com/diegowenndson/springboot/controller/OsController.java
+++ src/main/java/com/diegowenndson/springboot/controller/OsController.java
@@ -1,2 +1,44 @@
-package com.diegowenndson.springboot.controller;public class OsController {
+package com.diegowenndson.springboot.controller;
+
+import com.diegowenndson.springboot.DTO.OsDTO;
+import com.diegowenndson.springboot.domain.OS;
+import com.diegowenndson.springboot.service.OsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/os")
+public class OsController {
+
+    @Autowired
+    private OsService osService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<OsDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(new OsDTO(osService.findById(id)));
+
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<OsDTO>> findAll(){
+        return ResponseEntity.ok().body(osService.findAll()
+                .stream()
+                .map(obj -> new OsDTO(obj))
+                .collect(Collectors.toList()));
+    }
+
+    @PostMapping()
+    public  ResponseEntity<OsDTO> save(@Valid @RequestBody OsDTO os){
+        os = new OsDTO(osService.save(os));
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(os.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
 }
--- src/main/java/com/diegowenndson/springboot/service/OsService.java
+++ src/main/java/com/diegowenndson/springboot/service/OsService.java
@@ -1,2 +1,54 @@
-package com.diegowenndson.springboot.service;public class OsService {
+package com.diegowenndson.springboot.service;
+
+import com.diegowenndson.springboot.DTO.OsDTO;
+import com.diegowenndson.springboot.domain.Cliente;
+import com.diegowenndson.springboot.domain.OS;
+import com.diegowenndson.springboot.domain.Tecnico;
+import com.diegowenndson.springboot.domain.enuns.Prioridade;
+import com.diegowenndson.springboot.domain.enuns.Status;
+import com.diegowenndson.springboot.repository.OSRepository;
+import com.diegowenndson.springboot.service.Exceptions.ObjectNotFoundsExceptions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OsService {
+
+    @Autowired
+    private TecnicoService tecnicoService;
+    @Autowired
+    private OSRepository osRepository;
+    @Autowired
+    private ClienteService clienteService;
+
+    public OS findById(Integer id){
+        Optional<OS> obj = osRepository.findById(id);
+        return obj.orElseThrow(
+                () -> new ObjectNotFoundsExceptions("Os Não encotnrada id: " +id+ ", Tipo: "+OS.class.getName()));
+    }
+
+    public List<OS> findAll() {
+        return osRepository.findAll();
+    }
+
+    public OS save(@Valid OsDTO os) {
+        return fromDTO(os);
+    }
+
+    private OS fromDTO(OsDTO os){
+        OS newObj = new OS();
+        newObj.setId(os.getId());
+        newObj.setObservacoes(os.getObservacoes());
+        newObj.setPrioridade(Prioridade.toEnum(os.getPrioridade()));
+        newObj.setStatus(Status.toEnum(os.getStatus()));
+        Tecnico tecnico = tecnicoService.findById(os.getTecnico());
+        Cliente cliente = clienteService.findById(os.getCliente());
+        newObj.setTecnico(tecnico);
+        newObj.setCliente(cliente);
+        return osRepository.save(newObj);
+    }
 }
