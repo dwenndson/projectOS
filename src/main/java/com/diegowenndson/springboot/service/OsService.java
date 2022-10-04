af415497408a551168935bc770190e76bee73ac6
Diego Wenndson Souza Costa <CIANDT\dwenndson@lnb029358bhz.ciandt.global>
2022-10-04 09:49:30 -0300

    terminando serviço de OS

--- src/main/java/com/diegowenndson/springboot/service/OsService.java
+++ src/main/java/com/diegowenndson/springboot/service/OsService.java
@@ -7,11 +7,13 @@ import com.diegowenndson.springboot.domain.Tecnico;
 import com.diegowenndson.springboot.domain.enuns.Prioridade;
 import com.diegowenndson.springboot.domain.enuns.Status;
 import com.diegowenndson.springboot.repository.OSRepository;
+import com.diegowenndson.springboot.service.Exceptions.DataIntegratyViolationException;
 import com.diegowenndson.springboot.service.Exceptions.ObjectNotFoundsExceptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -39,6 +41,19 @@ public class OsService {
         return fromDTO(os);
     }
 
+    public OS update(OsDTO dto){
+        findById(dto.getId());
+        return fromDTO(dto);
+    }
+
+    public void delete(Integer id) {
+        OS os = findById(id);
+        if(os.getStatus().getCod().equals(2)){
+            throw new DataIntegratyViolationException("Os já finalizada não pode ser deletada");
+        }
+        osRepository.deleteById(id);
+    }
+
     private OS fromDTO(OsDTO os){
         OS newObj = new OS();
         newObj.setId(os.getId());
@@ -49,6 +64,10 @@ public class OsService {
         Cliente cliente = clienteService.findById(os.getCliente());
         newObj.setTecnico(tecnico);
         newObj.setCliente(cliente);
+
+        if(newObj.getStatus().getCod().equals(2)){
+            newObj.setDataFechamento(LocalDateTime.now());
+        }
         return osRepository.save(newObj);
     }
 }
